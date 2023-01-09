bf6fd5d6287bf19047d4d3f0eba5573e510b18cb
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-09 22:22:35 +0000

    test

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,27 @@
 import $ from "jquery";
+const _ = require('lodash');
+
 let count = 0
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append(`<button onClick="updateCounter()">Click here to get started</button>`);
-$('body').append(`<p id='count'></p>`);
-$('body').append('<p>Copyright - Holberton School</p>');
 
+/*
 function updateCounter(){
     count++
     $('#count').html(`${count} clicks on the button`);
 
-    
 }
+*/
+window.onload = function(){
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append(`<button>Click here to get started</button>`);
+    $('body').append(`<p id='count'></p>`);
+    $('body').append('<p>Copyright - Holberton School</p>');
+    $("button").on("click", function(){
+        alert("button");
+    }); 
+
+};
+
+
+
