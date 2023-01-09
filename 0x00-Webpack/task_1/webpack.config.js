a66521b7a9ee359e5291760229065cb94da938c0
Code Slayer <3613@holbertonschool.com>
2023-01-09 17:32:30 -0600

    task 1 does not run yet

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,11 +1,8 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
     mode: "production",
-    entry: {
-      main: path.resolve(__dirname, './js/dashboard_main.js'),
-    },
+    entry: './js/dashboard_main.js',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'public')
