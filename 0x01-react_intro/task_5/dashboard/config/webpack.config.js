e50c3b2eb53f935b2c751c285a1893f63ba97314
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-11 17:36:41 +0000

    task 5

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader", "file-loader", "image-webpack-loader"],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'babel-loader'
+        }
+      ],
+    },
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, '../dist'),
+      },
+      compress: true,
+      port: 8564,
+    },
+
+    devtool: 'inline-source-map',
+    chunks: ["all"]
+
+
+
+
+
+};
