f458fcb87f89a4276834b355016e57391bdb7205
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-18 19:10:29 +0000

    task 3

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+//import Notifications from './Notifications/Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+
+
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/*
+<div id="root-notifications">
+      <Notifications />
+    </div>
+*/
