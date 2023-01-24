d78c84cdc5e118eb28b9bf92a6651b7938aff6fd
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-24 22:28:43 +0000

    task 5

---
+++ 0x02-react_redux_action_creator_normalizr/task_5/dashboard/src/Login/Login.js
@@ -0,0 +1,109 @@
+import logo from '../assets/holberton_logo.jpg';
+import {getFullYear} from '../utils/utils.js';
+import {getFooterCopy} from '../utils/utils.js';
+import React from 'react';
+
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+
+  form:{
+      alignItems: 'left',
+       paddingRight: '400px',
+      display: 'inline',
+      width: '100px',
+      '@media (max-width: 900px)': {
+        maxWidth: '250px'
+      },
+      },
+    labelEmail: {
+      alignItems: 'left',
+      paddingRight: '400px',
+      display: 'inline',
+
+
+      '@media (min-width: 900px)': {
+      },
+
+    },
+    labelPassword: {
+      alignItems: 'left',
+      paddingRight: '400px',
+
+      '@media (min-width: 900px)': {
+      },
+
+    },
+    inputEmail: {
+      alignItems: 'left',
+      display: 'inline',
+
+      '@media (min-width: 900px)': {
+      },
+
+    },
+    inputPassword: {
+      alignItems: 'left',
+
+      '@media (min-width: 900px)': {
+      },
+
+    },
+
+    btn: {
+      marginTop: '30px',
+
+      '@media (min-width: 900px)': {
+      },
+
+    }
+  });
+
+function Login({ login }) {
+  const [state, setState] = React.useState({
+    email: '',
+    password: '',
+    enableSubmit: false
+  })
+
+  function handleLoginSubmit(event){
+    event.preventDefault()
+    login(state.email, state.password)
+  }
+
+  function handleChangeEmail(event){
+    if (state.email.length > 0 && state.password.length > 0){
+      state.enableSubmit = true
+    }
+    setState({...state, email: event.target.value})
+
+
+  }
+  function handleChangePassword(event){
+    if (state.email.length > 0 && state.password.length > 0){
+      state.enableSubmit = true
+    }
+    setState({...state,password: event.target.value})
+  }
+  
+  return (
+    <React.Fragment>
+        <p>Login to access the full dashboard</p>
+        <form className={css(styles.form)} onSubmit={handleLoginSubmit}>
+          <div>
+            <label className={css(styles.labelEmail)} for="email">email</label><br></br>
+            <input className={css(styles.inputEmail)} type="email" value={state.email} name="email" onChange={handleChangeEmail}></input>
+          </div>
+          <div>
+            <label className={css(styles.labelPassword)} for="password">password</label><br></br>
+            <input className={css(styles.inputPassword)} type="password" value={state.password} name="password" onChange={handleChangePassword}></input>
+          </div>
+          
+          <input type="submit" value="OK" className={css(styles.btn)} disabled={(state.enableSubmit === false)}/>
+        </form>
+        </React.Fragment>
+
+  );
+}
+
+export default Login;
---
+++ 0x02-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,11 @@
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+
+export function markAsAread(index){
+    return {type: MARK_AS_READ, index: index}
+}
+
+export function setNotificationFilter(filter){
+    return {type: SET_TYPE_FILTER, filter: filter}
+
+}
