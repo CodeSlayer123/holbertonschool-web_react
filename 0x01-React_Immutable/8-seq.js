2e72f89d9e09de932b2d69c47ed5275c3fa3a486
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-24 18:54:13 +0000

    task 8

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -6,7 +6,6 @@ export default function printBestStudents(object) {
   .map(x => {
     x.firstName = x.firstName.charAt(0).toUpperCase() + x.firstName.slice(1);
     x.lastName = x.lastName.charAt(0).toUpperCase() + x.lastName.slice(1);
-    return x
 });
   console.log(grades.toJS());
 }
