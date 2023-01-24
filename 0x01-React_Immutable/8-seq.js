d9823e78533a370250d77590935cce626a2219c0
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-24 18:41:33 +0000

    task 8

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -3,7 +3,7 @@ import { Seq } from 'immutable';
 export default function printBestStudents(object) {
   const grades = Seq(object)
   .filter(x => x.score >= 70)
-  .map(x => {
+  .map((x) => {
     x.firstName = x.firstName.charAt(0).toUpperCase() + word.slice(1);
     x.lastName = x.lastName.charAt(0).toUpperCase() + word.slice(1);
     return x
