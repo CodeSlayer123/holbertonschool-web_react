68ac411163fb75794377fc6d8ee4b027976d15cb
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-02 18:19:33 +0000

    task 0

---
+++ 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux';
+import { uiReducer as ui } from './reducers/uiReducer';
+import { courseReducer as course } from './reducers/courseReducer';
+import { notificationReducer as notifications } from './reducers/notificationReducer';
+import { Provider } from 'react-redux';
+//import Notifications from './Notifications/Notifications';
+
+const store = configureStore({ reducer: combineReducers({ui, course, notifications})})
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/*
+<div id="root-notifications">
+      <Notifications />
+    </div>
+*/
---
+++ 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/utils/utils.js
@@ -0,0 +1,18 @@
+function getFullYear(){
+  return new Date().getFullYear()
+}
+
+function getFooterCopy(isIndex){
+  if (isIndex === true){
+      return 'Holberton School'
+  }
+  if (isIndex === false){
+      return 'Holberton School main dashboard' 
+  }
+}
+
+function getLatestNotification(){
+  return `<strong>Urgent requirement</strong> - complete by EOD`
+}
+
+module.exports = {getFullYear, getFooterCopy, getLatestNotification }
