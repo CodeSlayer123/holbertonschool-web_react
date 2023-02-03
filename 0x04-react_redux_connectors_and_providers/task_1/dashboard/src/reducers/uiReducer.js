826015c38caf691de46353fdc52dc6bdaf2c0ff2
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-03 00:29:10 +0000

    task 1

---
+++ 0x04-react_redux_connectors_and_providers/task_1/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,50 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE} from '../actions/uiActionTypes';
+const { Map } = require('immutable');
+
+
+const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+})
+
+export function uiReducer(state = initialState, action){
+
+    switch (action.type) {
+        case DISPLAY_NOTIFICATION_DRAWER: {
+          return state.set({
+            ...state,
+            isNotificationDrawerVisible: true,
+          })
+        }
+        case HIDE_NOTIFICATION_DRAWER: {
+            return state.set({
+              ...state,
+              isNotificationDrawerVisible: false,
+            })
+          }
+          case LOGIN_SUCCESS: {
+            return state.set({
+              ...state,
+              isUserLoggedIn: true,
+            })
+          }
+          case LOGIN_FAILURE: {
+            return state.set({
+              ...state,
+              isUserLoggedIn: false,
+            })
+          }
+          case LOGOUT: {
+            return state.set({
+              ...state,
+              isUserLoggedIn: false,
+            })
+          }
+          default: {
+            return state
+          }
+    }
+
+
+}
