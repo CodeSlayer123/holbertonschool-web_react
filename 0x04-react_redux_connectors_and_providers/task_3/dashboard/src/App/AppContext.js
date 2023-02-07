754b61f2f01f26c1bd58f6df69ef2124dbd926c7
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-07 20:20:40 +0000

    task 3

---
+++ 0x04-react_redux_connectors_and_providers/task_3/dashboard/src/App/AppContext.js
@@ -0,0 +1,42 @@
+import React, { createContext } from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import uiReducer from '../reducers/uiReducer';
+
+const user = {
+    email: "",
+    password: "",
+    isLoggedIn: false
+}
+
+function logout(){
+    this.setState({
+        user: {
+            email: "",
+            password: "",
+            isLoggedIn: false
+        }
+    })
+
+    this.context.user = {
+        email: "",
+        password: "",
+        isLoggedIn: false
+
+    }
+}
+
+export const store = configureStore({
+    reducer: uiReducer,
+    preloadedState: {
+      ui: uiReducer(undefined, {}),
+    },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+        serializableCheck: false,
+    })
+  });
+  
+
+const AppContext = createContext({user, logout, store})
+
+export default AppContext
